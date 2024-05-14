81c10e7adc82545daed066a1c71d22e3dafbe66c
amirabas <amirjabic@gmail.com>
2024-05-14 03:35:45 +0330

    fixed fetch api not use react-query

--- src/pages/PostHero.page.tsx
+++ src/pages/PostHero.page.tsx
@@ -1,6 +1,25 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+type hero = {
+  id: number,
+  name : string
+}
 
 export default function PostHeroPage() {
+  const [data, setdata] = useState <hero[]>([])
+
+  useEffect(() => {
+    async function getFetching() {
+      await axios.get('http://localhost:4000/superhero').then(res => setdata(res.data))
+    }
+    getFetching()
+  }, [])
+
+
   return (
-    <div>PostHero.page</div>
+    <div  style={{padding: "10px",float:"left"}}>
+      {data.map(i => <h2 key={i.id}>{i.name}</h2>)}
+    </div>
   )
 }
