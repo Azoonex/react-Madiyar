e7edbaf56c20af37d822dfcc86173bc155e1d5c2
amirabas <amirjabic@gmail.com>
2024-05-26 10:43:00 +0430

    test btn for style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,7 @@ export default function Button({ style, text, Icon }: ButtonTypes) {
   return (
     <button className={style}>{text}
       {Icon && <span><Icon className="i-xs" /></span>}
+      <div className="items hello"></div>
     </button>
   )
 }
