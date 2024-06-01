8b6511ba13570236ae886ee8cef609a5982e6ee9
amirabas <amirjabic@gmail.com>
2024-06-02 01:13:05 +0330

    how work submiting finsh course 7

--- src/reackHook/Login.tsx
+++ src/reackHook/Login.tsx
@@ -1,13 +1,26 @@
 import { useForm, } from "react-hook-form"
 import { DevTool } from "@hookform/devtools"
+import { SubmitHandler } from "react-hook-form";
+
+type DataFormValue = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 export default function Login() {
 
-  const form = useForm()
-  const { register, control } = form
-  return (
 
-    <div>  <h1>YouTube Form</h1>
-      <form>
+
+  const { register, handleSubmit, control } = useForm < DataFormValue > ();
+
+  const onSubmit: SubmitHandler<DataFormValue> = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
 
