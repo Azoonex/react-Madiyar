f94dcb15e74b52df0d846aecc47d8563b374d19c
amirabas <amirjabic@gmail.com>
2024-06-25 12:16:18 +0330

    how work onchnaging for rfk

--- src/reackHook/Login.tsx
+++ src/reackHook/Login.tsx
@@ -1,20 +1,24 @@
+import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools";
 
 export default function Login() {
+  const {control,register,handleSubmit} = useForm()
   return (
     <div>
       <h1>YouTube Form</h1>
 
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username"  {...register("username")} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
+        <DevTool control={control} />
       </form>
     </div>
   )
