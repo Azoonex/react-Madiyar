69c31ff93f129ecf7fc80572b914d8272ea6a13b
amirabas <amirjabic@gmail.com>
2024-06-25 13:45:30 +0330

    how work validateion for secound value

--- src/reackHook/Login.tsx
+++ src/reackHook/Login.tsx
@@ -9,23 +9,49 @@ type FormValues = {
 
 
 export default function Login() {
-  const {control,register,handleSubmit} = useForm<FormValues>()
+  const form = useForm<FormValues>()
+  const { control, register, handleSubmit,formState } = form
+  const {errors} = formState
+
   const onSbmite = (data: FormValues)=>{
     console.log('Form submit',data);
   }
+
+
   return (
     <div>
       <h1>YouTube Form</h1>
 
       <form onSubmit={handleSubmit(onSbmite)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username"  {...register("username")} />
+        <input type="text" id="username"  {...register("username",{
+          required: 'UserName is required !',
+          validate: (value) => {
+            return (
+              value === 'amirabas' || 'not work this is name'
+            )
+          }
+        })} />
+        <p>{errors.username?.message}</p>
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <input type="email" id="email" {...register("email",{
+          pattern : {
+            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+            message: "Invaid in formate"
+          },
+          
+        })} />
+        <p>{errors.email?.message}</p>
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input type="text" id="channel" {...register("channel",{
+          required: {
+            value: true,
+            message: "This is not valid channel"
+          }
+        })} />
+        <p>{errors.channel?.message}</p>
 
         <button style={{marginTop: "10px"}}>Submit</button>
         <DevTool control={control} />
@@ -33,3 +59,6 @@ export default function Login() {
     </div>
   )
 }
+
+
+// calling valdtion for validate calling module
