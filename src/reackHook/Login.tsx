f6e69d693079cba7ad9c2cdc2de6de6ffdcd38f9
amirabas <amirjabic@gmail.com>
2024-06-25 12:23:07 +0330

    onsubmistion for rfc

--- src/reackHook/Login.tsx
+++ src/reackHook/Login.tsx
@@ -1,13 +1,23 @@
 import { useForm } from "react-hook-form"
 import { DevTool } from "@hookform/devtools";
 
+type FormValues = {
+  username: string,
+  email : string,
+  channel : string
+}
+
+
 export default function Login() {
-  const {control,register,handleSubmit} = useForm()
+  const {control,register,handleSubmit} = useForm<FormValues>()
+  const onSbmite = (data: FormValues)=>{
+    console.log('Form submit',data);
+  }
   return (
     <div>
       <h1>YouTube Form</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(onSbmite)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username"  {...register("username")} />
 
@@ -17,7 +27,7 @@ export default function Login() {
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
 
-        <button>Submit</button>
+        <button style={{marginTop: "10px"}}>Submit</button>
         <DevTool control={control} />
       </form>
     </div>
