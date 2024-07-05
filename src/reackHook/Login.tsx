721f2dc14a072ae354036c1e5631930da61e8551
amirabas <amirjabic@gmail.com>
2024-07-05 09:00:51 +0330

    how work watch and bast proformance

--- src/reackHook/Login.tsx
+++ src/reackHook/Login.tsx
@@ -1,31 +1,36 @@
-import { useForm } from "react-hook-form"
 import { DevTool } from "@hookform/devtools";
-
-type FormValues = {
-  username: string,
-  email : string,
-  channel : string,
-  age: number
-}
+import { FormValues } from "./types_form";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 
 export default function Login() {
-  const form = useForm<FormValues>()
-  const { control, register, handleSubmit,formState } = form
-  const {errors} = formState
+  const form = useForm < FormValues > ()
+  const { control, register, handleSubmit, formState, watch } = form
+  const { errors } = formState
 
-  const onSbmite = (data: FormValues)=>{
-    console.log('Form submit',data);
+  const onSbmite = (data: FormValues) => {
+    console.log('Form submit', data);
   }
 
+  const watchUserName = watch(["username","email"])
+
+  useEffect(()=>{
+    const subscribon = watch((value)=>{
+      console.log(value);
+    })
+
+    return ()=>{subscribon.unsubscribe()}
+
+  },[watch])
 
   return (
     <div>
       <h1>YouTube Form</h1>
-
+      <h2>watched value : {watchUserName}</h2>
       <form onSubmit={handleSubmit(onSbmite)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username"  {...register("username",{
+        <input type="text" id="username"  {...register("username", {
           required: 'UserName is required !',
           validate: (value) => {
             return (
@@ -36,17 +41,17 @@ export default function Login() {
         <p>{errors.username?.message}</p>
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email",{
-          pattern : {
+        <input type="email" id="email" {...register("email", {
+          pattern: {
             value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
             message: "Invaid in formate"
           },
-          
+
         })} />
         <p>{errors.email?.message}</p>
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel",{
+        <input type="text" id="channel" {...register("channel", {
           required: {
             value: true,
             message: "This is not valid channel"
@@ -56,7 +61,7 @@ export default function Login() {
 
         <label htmlFor="age">Age</label>
         <input type="number" id="age" {...register("age", {
-          valueAsNumber:true,
+          valueAsNumber: true,
           required: {
             value: true,
             message: "This is not valid Age"
@@ -65,7 +70,7 @@ export default function Login() {
         <p>{errors.age?.message}</p>
 
 
-        <button style={{marginTop: "10px"}}>Submit</button>
+        <button style={{ marginTop: "10px" }}>Submit</button>
         <DevTool control={control} />
       </form>
     </div>
---
+++ src/reackHook/types_form.ts
@@ -0,0 +1,6 @@
+export type FormValues = {
+    username: string;
+    email: string;
+    channel: string;
+    age: number;
+};
